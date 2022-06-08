2cc8c9b1a6a059ebd96d845c9c9e5ca0cd61c389
MR-DHRUV <dhruvgupta742@gmail.com>
2022-06-08 20:56:22 +0530

    Login signup added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import './assets/bootstrap/css/bootstrap.min.css'
+import Login from './components/Login';
+import {
+  HashRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+import SignUp from './components/SignUp';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+
+    <Switch>
+    
+    <Route exact path='/signup'> <SignUp /> </Route>
+    <Route exact path='/'> <h1>Hello</h1> </Route>
+    <Route exact path='/signin'> <Login /> </Route>
+    </Switch>
+
+
+    </Router>
   );
 }
 
