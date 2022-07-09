526359bbe4e8c3ceda100911879bcccc1df7305f
Ayush Baliyan <ayush.3316403221@ipu.ac.in>
2022-07-09 12:29:07 +0530

    Navbar svg fixed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -31,7 +31,7 @@ const Navbar = (props) => {
             data-bs-target="#navcol-1"
           >
             <span className="visually-hidden">Toggle navigation</span>
-            <span id="myNavTog"><svg xmlns="http://www.w3.org/2000/svg" width="" height="40" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+            <span id="myNavTog"><svg xmlns="http://www.w3.org/2000/svg" width="" height="40" class="bi bi-list" id="nav-svg" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
             </svg></span>
           </button>
